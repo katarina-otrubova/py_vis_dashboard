c18c844959e05a1ad7a7bb6f3b9d87940f13c9f3
Katarina Pokryvkova <katarina.pokryvkova@zurich.com>
2025-03-05 10:03:36 +0100

    create container with triangle plots

---
+++ container_triangles.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+def create_triangle_tab(data):
+    container_triangle = st.container()
+
+    st.write("TRIANGLE TAB")
+
+    # Toggle for Incurred vs Paid
+    view_label = st.radio(
+        "Select View",
+        options=['Incurred', 'Paid'],
+        index=0
+    )
+
+    # LoB selector
+    unique_lobs = data['lob'].unique()
+    selected_lobs = st.multiselect(
+        "Select LOB", 
+        options = unique_lobs, 
+        default = unique_lobs  
+    )
+
+    if view_label == 'Incurred':
+        view_value = 'inc'
+    else:
+        view_value = 'paid'
+
+    df_filtered = data[(data['trs'] == view_value) & 
+                       (data['lob'].isin(selected_lobs))
+                       ]
+    
+    df_pivot = df_filtered.pivot_table(index='dev', columns='year', values='value')
+
+    # Plot the chart using Streamlit
+    # st.line_chart(df_pivot)
+
+
+    # Plot with Matplotlib instead
+    fig, ax = plt.subplots(figsize=(8, 4))
+    for column in df_pivot.columns:
+        series = df_pivot[column]#.dropna()
+        ax.plot(
+            series.index, 
+            series, 
+            markersize = 3, 
+            marker = 'o', 
+            label = column
+            )
+        
+    # for column in df_pivot.columns:
+    # column = df_pivot.columns[-1]
+    # series = df_pivot[column].dropna()
+    # ax.plot(
+    #     series.index, 
+    #     series, 
+    #     markersize = 3, 
+    #     marker = 'o', 
+    #     label = column
+    #     )
+
+    # Set x-axis to show integer values only
+    ax.set_xticks(range(0, int(df_pivot.index.max()) + 1))
+    ax.set_xlabel('Development Year')
+    ax.set_ylabel('Value')
+    ax.set_title(f'Development {view_label}')
+    ax.legend(title='Year', loc='center left', bbox_to_anchor=(1, 0.5))
+    max_value = df_pivot.max().max()
+    ax.set_ylim(0, max_value * 1.1)
+
+
+    # Display the plot using Streamlit
+    st.pyplot(fig)
+
+
+    # alernatively more columns
+    # col1, col2 = st.columns([2, 3])  # Adjust the ratio for your needs
+
+    # with col1:
+    #     st.pyplot(fig)  # Display the plot in the first column
+
+    # with col2:
+    #     st.write("Additional content can go here.")  # Additional content in the second column
