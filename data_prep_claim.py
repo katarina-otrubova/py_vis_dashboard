f0a44497c597296e0ec066d22c3daa20ebef8e37
Katarina Pokryvkova <katarina.pokryvkova@zurich.com>
2025-03-06 14:36:54 +0100

    prepare claim data by subsetting relevant columns

--- data_prep_claim.py
+++ data_prep_claim.py
@@ -8,5 +8,36 @@ from ydata_profiling import ProfileReport
 
 dt_customer_raw = pd.read_csv('data/Main.csv')
 
-dt_customer = dt_customer_raw
+# profile = ProfileReport(dt_customer_raw, title="Pandas Profiling Report")
+# profile
 # %%
+
+dt_claims = dt_customer_raw[[
+    'ClaimNb',
+    'Exposure',
+    'Country',
+    'InsuranceType',
+    'premiums',
+    'Claims',
+    'Expenses'
+    ]]
+
+
+dt_claims.loc[dt_claims['ClaimNb'] == 0, ['Claims', 'Expenses']] = 0
+
+
+
+
+
+
+dt_customer = dt_customer_raw
+
+
+
+
+# notes
+# try delete loss if claim count= 0
+# freq = sum(claim count) / sum(exposure)
+
+# bonus malus = extra charge based on claim exp
+
