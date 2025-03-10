12a151d9d87e8689aa11817e720588ad4d95a0dd
Katarina Pokryvkova <katarina.pokryvkova@zurich.com>
2025-03-10 09:23:44 +0100

    add some more plots

--- container_motor.py
+++ container_motor.py
@@ -7,7 +7,7 @@ import plotly.figure_factory as ff
 def create_motor_tab(data):
 
     container_motor = st.container()
-    st.subtitle("Geographical distribution of claims")
+    st.subheader("Geographical distribution of claims")
 
     selector_1, selector_2 = st.columns([1, 2]) 
     with selector_1:
@@ -22,15 +22,10 @@ def create_motor_tab(data):
     with selector_2:
         pass
 
-
-
-
     df_filtered = data[
         (data['Claims'] >= selected_size_range[0]) &
         (data['Claims'] <= selected_size_range[1])
         ]
-
-
 
     fig = go.Figure(go.Scattergeo(
         lat=df_filtered['latitude'],
@@ -39,7 +34,7 @@ def create_motor_tab(data):
         marker=dict(
             size=12,
             color=df_filtered['Claims'],
-            colorscale='Viridis',
+            colorscale='sunsetdark',
             showscale=True
         ),
         text='',
@@ -62,12 +57,30 @@ def create_motor_tab(data):
 
 
 
-# # Claim distribution
-    st.subtitle("Size distribution of claims")
+  # Claim distribution
+    st.subheader("Size distribution of claims")
     hist_data = [data['Claims']]
     group_labels = ['Incurred amount']
 
-    fig_dist = ff.create_distplot(hist_data, group_labels)
+    fig_dist = ff.create_distplot(hist_data, group_labels, colors =['tomato'])
     st.plotly_chart(fig_dist)
 
 
+    # Adding three columns with different plots
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.subheader("Vehicle Power")
+        fig_box = go.Figure(go.Box(y=data['VehPower'], name="VehPower", marker_color='firebrick'))
+        st.plotly_chart(fig_box)
+
+    with col2:
+        st.subheader("Policy Holders Age Distribution")
+        fig_hist = go.Figure(go.Histogram(x=data['Policy holders age'], marker_color='indianred'))
+        st.plotly_chart(fig_hist)
+
+    with col3:
+        st.subheader("Average incurred per Wealth Group")
+        df_sum_claims = data.groupby('Wealth Group')['Claims'].mean().reset_index()
+        fig_bar = go.Figure(go.Bar(x=df_sum_claims['Wealth Group'], y=df_sum_claims['Claims'], marker_color='tomato'))
+        st.plotly_chart(fig_bar)
